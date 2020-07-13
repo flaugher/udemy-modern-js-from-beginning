66ab178837aa760a7c9d64247808d6b9034fefa7
Robert Flaugher <rlfprog@gmail.com>
2020-07-13 15:21:15 -0700

    Add Page State example

--- 11_7_project_files/js_sandbox/app.js
+++ 11_7_project_files/js_sandbox/app.js
@@ -1,85 +1,79 @@
-const PageState = function() {
+// Page state
+const PageState = function () {
+  // 'this' refers to the page state
   let currentState = new homeState(this);
 
-  this.init = function() {
+  // Initialize page state.
+  this.init = function () {
     this.change(new homeState);
   }
 
-  this.change = function(state) {
+  // Change the current state.
+  this.change = function (state) {
     currentState = state;
   }
 };
 
-// Home State
-const homeState = function(page) {
-  document.querySelector('#heading').textContent = null;
-  document.querySelector('#content').innerHTML = `
+// Home state
+const homeState = function (page) {
+  document.querySelector('#heading').textContent = null;  // We don't want a heading, just the Jumbotron.
+  document.querySelector('#heading').innerHTML = `
     <div class="jumbotron">
-    <h1 class="display-3">Hello, world!</h1>
+    <h1 class="display-4">Hello, world!</h1>
     <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
     <hr class="my-4">
     <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-    <p class="lead">
-      <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-    </p>
-  </div>
-  `;
-};
+    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </div>
+  `
+}
 
-// About State
-const aboutState = function(page) {
+// About state
+const aboutState = function (page) {
   document.querySelector('#heading').textContent = 'About Us';
   document.querySelector('#content').innerHTML = `
-    <p>This is the about page</p>
-`;
-};
+  <p>This is the About page</p>
+  `;
+}
 
-// Contact State
-const contactState = function(page) {
+// Contact state
+const contactState = function (page) {
   document.querySelector('#heading').textContent = 'Contact Us';
   document.querySelector('#content').innerHTML = `
-  <form>
-    <div class="form-group">
-      <label>Name</label>
-      <input type="text" class="form-control">
+    <form>
+      <div class="form-group">
+        <label>Name</label>
+        <input type="text" class="form-control">
+      </div>
+      <div class="form-group">
+      <label>Email address</label>
+      <input type="email" class="form-control">
     </div>
-    <div class="form-group">
-    <label>Email address</label>
-    <input type="email" class="form-control">
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-  </form>
-`;
-};
+    <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  `;
+}
 
-// Instantiate pageState
-const page = new PageState();
-
-// Init the first state
-page.init();
-
-// UI Vars
+// Page state event listeners
 const home = document.getElementById('home'),
-      about = document.getElementById('about'),
-      contact = document.getElementById('contact');
+  about = document.getElementById('about'),
+  contact = document.getElementById('contact');
 
-// Home
-home.addEventListener('click', (e) => {
+home.addEventListener('click', (event) => {
   page.change(new homeState);
+  event.preventDefault();
+})
 
-  e.preventDefault();
-});
-
-// About
-about.addEventListener('click', (e) => {
-  page.change(new aboutState);
-
-  e.preventDefault();
-});
-
-// Contact
-contact.addEventListener('click', (e) => {
+contact.addEventListener('click', (event) => {
   page.change(new contactState);
+  event.preventDefault();
+})
 
-  e.preventDefault();
-});
+about.addEventListener('click', (event) => {
+  page.change(new aboutState);
+  event.preventDefault();
+})
+
+// Page controller
+const page = new PageState();
+page.init();
