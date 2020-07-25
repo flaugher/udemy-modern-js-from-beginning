80069d49ba5e7dae0025fc1385074003e76c18f1
Robert Flaugher <rlfprog@gmail.com>
2020-07-25 16:14:26 -0700

    Write number guesser losing and game over cases

--- solutions/4_9_project_files/numberguesser/app.js
+++ solutions/4_9_project_files/numberguesser/app.js
@@ -9,33 +9,33 @@ GAME FUNCTION:
 
 // Game values
 let min = 1,
-    max = 10,
-    winningNum = 2,
-    guessesLeft = 3;
+  max = 10,
+  winningNum = 2,
+  guessesLeft = 3;
 
 // UI Elements
 const game = document.querySelector('#game'),
-      minNum = document.querySelector('.min-num'),
-      maxNum = document.querySelector('.max-num'),
-      guessBtn = document.querySelector('#guess-btn'),
-      guessInput = document.querySelector('#guess-input'),
-      message = document.querySelector('.message');
+  minNum = document.querySelector('.min-num'),
+  maxNum = document.querySelector('.max-num'),
+  guessBtn = document.querySelector('#guess-btn'),
+  guessInput = document.querySelector('#guess-input'), ;
+message = document.querySelector('.message');
 
 // Assign UI min and max
 minNum.textContent = min;
 maxNum.textContent = max;
-      
+
 // Listen for guess
-guessBtn.addEventListener('click', function(){
+guessBtn.addEventListener('click', function () {
   let guess = parseInt(guessInput.value);
-  
+
   // Validate
-  if(isNaN(guess) || guess < min || guess > max){
+  if (isNaN(guess) || guess < min || guess > max) {
     setMessage(`Please enter a number between ${min} and ${max}`, 'red');
   }
 
   // Check if won
-  if(guess === winningNum){
+  if (guess === winningNum) {
     // Game over - won
     gameOver(true, `${winningNum} is correct, YOU WIN!`);
 
@@ -43,7 +43,7 @@ guessBtn.addEventListener('click', function(){
     // Wrong number
     guessesLeft -= 1;
 
-    if(guessesLeft === 0){
+    if (guessesLeft === 0) {
       // Game over - lost
       gameOver(false, `Game Over, you lost. The correct number was ${winningNum}`);
     } else {
@@ -62,7 +62,7 @@ guessBtn.addEventListener('click', function(){
 });
 
 // Game over
-function gameOver(won, msg){
+function gameOver(won, msg) {
   let color;
   won === true ? color = 'green' : color = 'red';
 
@@ -77,7 +77,7 @@ function gameOver(won, msg){
 }
 
 // Set message
-function setMessage(msg, color){
+function setMessage(msg, color) {
   message.style.color = color;
   message.textContent = msg;
 }
